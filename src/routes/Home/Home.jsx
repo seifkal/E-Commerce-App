c5e77cb45264a005e410925eb70e3e5dc3ddd044
Seifkall <saifkall5@gmail.com>
2025-07-22 12:56:45 +0300

    make home banner paragraph unselectable

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -7,9 +7,9 @@ export default function Home(){
             <div className="flex flex-col items-start justify-center h-full pl-16 relative">
                 <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent z-0"></div>
                 <div className="relative z-10">
-                    <p className="text-white font-extrabold text-6xl font-space-grotesk text-left">One Store.</p>
-                    <p className="text-white font-extrabold text-6xl font-space-grotesk text-left mb-6">Endless Choices.</p>
-                    <Link to="/shop" className="text-white font-extrabold text-xl font-space-grotesk text-left border p-2">SHOP NOW</Link>
+                    <p className="text-white font-extrabold text-6xl font-space-grotesk text-left select-none">One Store.</p>
+                    <p className="text-white font-extrabold text-6xl font-space-grotesk text-left mb-6 select-none">Endless Choices.</p>
+                    <Link to="/shop" className="text-white font-extrabold text-xl font-space-grotesk text-left border p-2 select-none">SHOP NOW</Link>
                 </div>
             </div>
         </div>
